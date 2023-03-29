9b3a2d6bf856f6329793c93f4345e29c1359b756
ㅡApfelsaft-ASAP <myoungbeen@gmail.com>
2023-03-29 21:19:32 +0800

    fix : 인준님 흔적 삭제

--- src/main/java/com/example/someonebe/repository/BoardFileRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.someonebe.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface BoardFileRepository extends JpaRepository<BoardFile, UUID> {
-}
